a858b343e95a32ac03a66459198542a2e47570af
hiroyannnn <hiroyannnn@gmail.com>
2023-05-03 14:57:19 +0900

    _currency

--- tdd/202305/src/Money/Dollar.ts
+++ tdd/202305/src/Money/Dollar.ts
@@ -1,13 +1,16 @@
 import { Money } from "./Money";
 
 export class Dollar extends Money {
+  private _currency: string = "";
+
   constructor(amount: number) {
     super();
     this.amount = amount;
+    this._currency = "USD";
   }
 
   currency(): string {
-    return "USD";
+    return this._currency;
   }
 
   times(multiplier: number): Money {
--- tdd/202305/src/Money/Franc.ts
+++ tdd/202305/src/Money/Franc.ts
@@ -1,13 +1,16 @@
 import { Money } from "./Money";
 
 export class Franc extends Money {
+  private _currency: string = "";
+
   constructor(amount: number) {
     super();
     this.amount = amount;
+    this._currency = "CHF";
   }
 
   currency(): string {
-    return "CHF";
+    return this._currency;
   }
 
   times(multiplier: number): Money {
