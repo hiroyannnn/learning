e413cc097c9974fdab587b2e5426611d0da7aed2
hiroyannnn <hiroyannnn@gmail.com>
2023-05-03 14:16:48 +0900

    timesの戻り値を変更

--- tdd/202305/TODO.md
+++ tdd/202305/TODO.md
@@ -1,7 +1,7 @@
 - [ ] TODO: $5 + 10CHF = $10 (レートが 2:1 の場合)
 - [x] TODO: $5 \*2 = $10
 - [ ] TODO: amount を private にする
-- [ ] TODO: dollar の副作用をどうする？
+- [x] TODO: dollar の副作用をどうする？
 - [ ] TODO: Money の丸め処理どうする？
 -
 - [x] Dollar クラスがない
--- tdd/202305/src/Money.ts
+++ tdd/202305/src/Money.ts
@@ -1,9 +1,11 @@
 export class Dollar {
   public amount: number = 0;
+
   constructor(amount: number) {
     this.amount = amount;
   }
-  times(multiplier: number): void {
-    this.amount *= multiplier;
+
+  times(multiplier: number): Dollar {
+    return new Dollar(this.amount * multiplier);
   }
 }
